e2da50de78a9fb10d86048565a1aa870e88cd054
Marek Wolski <marek.wolski@gmail.com>
2021-02-06 11:41:23 +0000

    Fixed blank screen problem
    Got the error handling right to sort the blank screen issue on the inside display. Added logging output. Retrieved recent stats csv files from pioutside.

--- inside/scrolltemp.py
+++ inside/scrolltemp.py
@@ -1,4 +1,4 @@
-import sys, time, scrollphat
+import sys, time, logging, scrollphat
 import requests, json
 
 def scroll_once():
@@ -10,24 +10,34 @@ def scroll_once():
 def get_temp():
     url = 'http://192.168.1.17:8080/json'
     try:
+        logging.info('Get updated stats ...')
         r = requests.get(url)
-        stats = json.loads(r.content)
-        #print(stats)
-        the_temp = f"{stats['temp']:.1f}"
-        the_pressure = f"{stats['pressure']:.0f}"
     except:
+        logging.warning('Tried to get the json stats and now in the exception code')
         the_temp = "Error getting temp!"
         the_pressure = "Error getting pressure!"
-    print('Temperature is ', the_temp, ' & the Pressure is ', the_pressure)
+    else:
+        stats = json.loads(r.content)
+        logging.info(stats)
+        the_temp = f"{stats['temp']:.1f}"
+        the_pressure = f"{stats['pressure']:.0f}"
+
     the_stats = the_temp + 'C  ' + the_pressure + 'hPa'
     return the_stats
 
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    filename='scrolltemp.log')
 
 scrollphat.set_brightness(2)
 scrollphat.set_rotate(True)
+logging.info('SCROLLING STARTED ...')
 
 while True:
     scrollphat.clear()
-    scrollphat.write_string(get_temp(), 11)
+    scrolltext = get_temp()
+    scrollphat.write_string(scrolltext, 11)
+    logging.info('Scroll this: %s', scrolltext)
     for i in range(6):
         scroll_once()
