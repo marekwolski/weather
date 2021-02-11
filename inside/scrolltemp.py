459f00a1efc58bd4170fb8822aec945149a95595
Marek Wolski <marek.wolski@gmail.com>
2021-02-11 20:07:36 +0000

    Fixed bug with the fetch error message
    ... and increased number of scrolls from 6 to 10.

--- inside/scrolltemp.py
+++ inside/scrolltemp.py
@@ -32,15 +32,12 @@ def get_temp():
         r = requests.get(url)
     except:
         logging.warning('Tried to get the json stats but now in the exception code')
-        the_temp = "Error getting temp!"
-        the_pressure = "Error getting pressure!"
+        the_stats = "Error getting temp! Error getting pressure!"
     else:
         stats = json.loads(r.content)
         logging.info(stats)
-        the_temp = f"{stats['temp']:.1f}"
-        the_pressure = f"{stats['pressure']:.0f}"
+        the_stats = f"{stats['temp']:.1f}" + 'c  ' + f"{stats['pressure']:.0f}" + 'hPa'
 
-    the_stats = the_temp + 'c  ' + the_pressure + 'hPa'
     scrollphathd.clear()                         # Clear the display and reset scrolling to (0, 0)
     scrollphathd.show()                          # Show the result
     return the_stats
@@ -57,5 +54,5 @@ logging.info('SCROLLING STARTED ...')
 while True:
     scrolltext = get_temp()
     logging.info('Scroll this: %s', scrolltext)
-    for i in range(6):
+    for i in range(10):
         scroll_message(scrolltext)
